f4a5824da94e68dec1a9f14cab0b6fd94ed15c22
Bogdan Parubok <bogdan.parubok@hotmail.com>
2024-03-03 19:35:06 +0200

    improves simple interest calculation.

--- interest-calculator.js
+++ interest-calculator.js
@@ -1,15 +1,24 @@
 function Calculate(id) {
     // Laenusummalt intressi arvutamine
-    let init = Number(document.getElementById(id[0]).value);                // algne summa
-    let interest = Number(document.getElementById(id[1]).value / 100);      // intressimäär
-    let timespan = Number(document.getElementById(id[2]).value);            // ajavahemik kuudes
-    let totalinterest = 0;
+    let init = Number(document.getElementById(id.algsumma).value);                      // algne summa
+    let interest = Number(document.getElementById(id.intressimaar).value / 100);        // intressimäär
+    let timespan = Number(document.getElementById(id.periood).value);                   // ajavahemik kuudes
+    let additions = Number(document.getElementById(id.lisatasu).value);                 // igakuine lisatasu/hooldustasu
+    let total_interest = 0;
 
     let total = (init * interest ) / timespan;
-    totalinterest = total * timespan;
+    total_interest = total * timespan;
 
-    document.getElementById(id[3]).value = init + totalinterest;
-    document.getElementById(id[4]).value = totalinterest;
+    let total_additions = additions * timespan;
+
+    let full_total = init + total_interest + total_additions;
+
+    document.getElementById(id.algsumma2).value = init;
+    document.getElementById(id.loppsumma).value = full_total
+    document.getElementById(id.intressisumma).value = total_interest;
+    document.getElementById(id.kogulisatasu).value = total_additions;
+
+    document.getElementById(id.vahe).value = (full_total - init).toFixed(2);
 
     // Laenujäägilt intressi arvutamine
 }
