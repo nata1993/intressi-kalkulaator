e3f1faca685e353845cc0b8c862923f335f84ea4
Bogdan Parubok <bogdan.parubok@hotmail.com>
2024-03-13 20:54:09 +0200

    Changes to functions calling.

--- interest-calculator.js
+++ interest-calculator.js
@@ -1,22 +1,53 @@
-function Calculate(id) {
+function Calculate() {
+
+
+
+    laenusummalt ();
+    laenujäägilt ();
+}
+
+function laenusummalt () {
     // Laenusummalt intressi arvutamine
-    let algsumma = Number(document.getElementById(id.algsumma).value);                 // algne summa
-    let intress = Number(document.getElementById(id.intressimaar).value / 100);        // intressimäär
-    let ajavahemik = Number(document.getElementById(id.periood).value);                // ajavahemik kuudes
-    let lepingutasu = Number(document.getElementById(id.lepingutasu).value);           // ajavahemik kuudes
-    let lisatasu = Number(document.getElementById(id.lisatasu).value);                 // igakuine lisatasu/hooldustasu
+    let laenusummalt_algsumma = Number(document.getElementById('algsumma').value);                  // algne summa
+    let intress = Number(document.getElementById('intressimaar').value / 100);         // intressimäär
+    let ajavahemik = Number(document.getElementById('periood').value);                 // ajavahemik kuudes
+    let lepingutasu = Number(document.getElementById('lepingutasu').value);            // ajavahemik kuudes
+    let lisatasu = Number(document.getElementById('lisatasu').value);                  // igakuine lisatasu/hooldustasu
 
-    let igakuine_intress = (algsumma * intress ) / ajavahemik;
-    let koguintress = igakuine_intress * ajavahemik;
-    let kogulisatasud = lisatasu * ajavahemik;
-    let kogusumma = algsumma + koguintress + kogulisatasud;
+    let laenusummalt_igakuine_intress = (laenusummalt_algsumma * intress ) / ajavahemik;
+    let laenusummalt_koguintress = laenusummalt_igakuine_intress * ajavahemik;
+    let laenusummalt_kogulisatasud = lisatasu * ajavahemik;
+    let laenusummalt_kogusumma = laenusummalt_algsumma + laenusummalt_koguintress + laenusummalt_kogulisatasud + lepingutasu;
 
-    document.getElementById(id.algsumma2).value = algsumma;
-    document.getElementById(id.loppsumma).value = kogusumma;
-    document.getElementById(id.intressisumma).value = koguintress;
-    document.getElementById(id.kogulisatasu).value = kogulisatasud;
+    document.getElementById('laenusummalt_algsumma').value = laenusummalt_algsumma;
+    document.getElementById('laenusummalt_loppsumma').value = laenusummalt_kogusumma;
+    document.getElementById('laenusummalt_intressisumma').value = laenusummalt_koguintress;
+    document.getElementById('laenusummalt_kogulisatasu').value = laenusummalt_kogulisatasud;
 
-    document.getElementById(id.vahe).value = (kogusumma - algsumma).toFixed(2);
+    document.getElementById('laenusummalt_vahe').value = (laenusummalt_kogusumma - laenusummalt_algsumma).toFixed(2);
+}
 
+function laenujäägilt () {
     // Laenujäägilt intressi arvutamine
+    let algsumma = Number(document.getElementById('algsumma').value);                  // algne summa
+    let intress = Number(document.getElementById('intressimaar').value / 100);         // intressimäär
+    let ajavahemik = Number(document.getElementById('periood').value);                 // ajavahemik kuudes
+    let lepingutasu = Number(document.getElementById('lepingutasu').value);            // ajavahemik kuudes
+    let lisatasu = Number(document.getElementById('lisatasu').value);                  // igakuine lisatasu/hooldustasu
+
+    let laenujäägilt_algsumma = algsumma;
+    let igakuine_põhimakse = algsumma / ajavahemik;
+    let laenujäägilt_intressisumma = 0;
+    let laenujäägilt_kogulisatasud = 0;
+    let laenujäägilt_igakuine_intress = 0;
+    for(let i = ajavahemik; i > 0; i--) {
+        laenujäägilt_intressisumma += algsumma * intress / ajavahemik;
+        laenujäägilt_algsumma - igakuine_põhimakse;
+    }
+
+    let laenujäägilt_kogusumma = laenujäägilt_algsumma + laenujäägilt_intressisumma + lepingutasu + lisatasu;
+
+    document.getElementById('laenujäägilt_algsumma').value = laenujäägilt_algsumma;
+
+    document.getElementById('laenujäägilt_vahe').value = laenujäägilt_intressisumma;
 }
